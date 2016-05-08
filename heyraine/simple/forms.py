a1d4b17935bd59a8102f13d0d505bf298e59d609
ffarhour <f.farhour@gmail.com>
2016-05-08 09:36:35 -0700

    backup

--- heyraine/simple/forms.py
+++ heyraine/simple/forms.py
@@ -2,3 +2,5 @@ from django import forms
 
 class EmailForm(forms.Form):
     email = forms.CharField(label='Your email', max_length=100,required=True,widget=forms.TextInput(attrs={'class':'form-control','style' : 'height:5vh'}) )
+class StartForm(forms.Form):
+    start = forms.CharField(label='start', max_length=100,required=True,widget=forms.TextInput(attrs={'class':'form-control','style' : 'height:5vh'}) )
--- heyraine/simple/views.py
+++ heyraine/simple/views.py
@@ -7,7 +7,7 @@ from django.core.urlresolvers import reverse
 import subprocess, sys, os
 from simple.models import room as room_DB
 
-from .forms import EmailForm
+from .forms import EmailForm, StartForm
 
 
 import room
@@ -44,30 +44,58 @@ def raine(request):
     response.write(u'<h1>Hi {0}</h1>'+email.format(hello))
     return response
     """
-    #request argument: email inputted by user
-    email = request.session['email']
 
-    #create room, add user
-    auth = "NmQ5MGQ0NzYtOWI4OS00ODc1LTgyYmItZGE2YzdhMjU1OGM2NDQ0OWVhYmEtNGJl"
-    headers = {"Authorization":"Bearer " + auth}
-    room_json = room.makeRoom(headers)
-    roomID = (room_json['id'])
-    person_json = room.addPerson(roomID,email)
-    personID = person_json['personId']
+    # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = StartForm(request.POST)
+        # check whether it's valid:
 
-    template = loader.get_template('simple/raine.html')
-    context = Context({"room_url": "https://web.ciscospark.com/#/rooms/","roomID":roomID})
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            start = form.cleaned_data['start']
+            # redirect to a new URL:
+            #url = reverse('raine', kwargs={'email': email})
+            request.session['start'] = start
 
-    rmdb = room_DB(roomID=roomID, personID=personID)
-    rmdb.save()
-    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    sys.path.append(BASE_DIR)
-    #subprocess.Popen(['Python', os.path.join(BASE_DIR, 'simple/room_miniscript.py')])
+            room_ID = room_DB.objects.get('roomID')
+            person_ID = room_DB.objects.get('personID')
 
-    p = subprocess.Popen(['Python', os.path.join(BASE_DIR, 'simple/room_miniscript.py'+' -r '+roomID+' -p '+personID)],stdin=subprocess.PIPE,shell=True)
-    p.communicate(roomID+'\n'+personID)[0]
+            count = 0
+            room.sendFirstMessage(room_ID)
+            #while (count < 10): #run 3 times. 3 responses from user,
+                                #3 responses from Watson.
+            messages_json = room._getMessages(room_ID, person_ID)
+            personMessage = (messages_json['items'][0]['text'])
+            room.sendMessage(room_ID, personMessage)
+            count += 1
 
-    #delete room
-    #room.deleteRoom(roomID, room_json)
+            return HttpResponseRedirect('')
 
-    return HttpResponse(template.render(context))
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = StartForm()
+        #request argument: email inputted by user
+        email = request.session['email']
+
+        #create room, add user
+        auth = "NmQ5MGQ0NzYtOWI4OS00ODc1LTgyYmItZGE2YzdhMjU1OGM2NDQ0OWVhYmEtNGJl"
+        headers = {"Authorization":"Bearer " + auth}
+        room_json = room.makeRoom(headers)
+        room_id = (room_json['id'])
+        person_json = room.addPerson(room_id,email)
+        person_id = person_json['personId']
+
+        template = loader.get_template('simple/raine.html')
+        context = Context({"room_url": "https://web.ciscospark.com/#/rooms/","roomID":room_id})
+
+        rmdb = room_DB(roomID=room_id, personID=person_id)
+        rmdb.save()
+
+        #p = subprocess.Popen(['Python', os.path.join(BASE_DIR, 'simple/room_miniscript.py'+' -r '+roomID+' -p '+personID)],stdin=subprocess.PIPE,shell=True)
+        #p.communicate(roomID+'\n'+personID)[0]
+
+        #delete room
+        #room.deleteRoom(roomID, room_json)
+
+        return HttpResponse(template.render(context))
